2e943db023729dc2a465bb4fc1cf3eac6f52900d
SUII07 <sujalbasnet2125@gmail.com>
2023-05-09 13:25:37 +0545

    Web host by sujal

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("parent").innerHTML=("something Else")          
+//  Camel Case naming Converction
+// document.getElementById("parent").style.backgroundColor=("red");
+document.getElementById("parent").style.cssText=("background-color:red; color:black; text-align:center");
+document.getElementsByClassName('exercise')[0].innerHTML=("I am class selector")
